47f217b62b21d1f5d4b158d65df1e56275ce8252
clutchfd <muh.hilman@gmail.com>
2015-05-22 18:31:32 +0700

    inside page html

--- HTML/js/style.js
+++ HTML/js/style.js
@@ -48,6 +48,44 @@ $(document).ready(function() {
 	  $('[data-toggle="tooltip"]').tooltip()
 	})
 	
+	
+	// CONTACT Form
+	$('#contact-form').bootstrapValidator({
+//        live: 'disabled',
+		message: 'This value is not valid',
+		feedbackIcons: {
+			valid: 'glyphicon glyphicon-ok',
+			invalid: 'glyphicon glyphicon-remove',
+			validating: 'glyphicon glyphicon-refresh'
+		},
+		fields: {
+			Name: {
+				validators: {
+					notEmpty: {
+						message: 'The Name is required and cannot be empty'
+					}
+				}
+			},
+			email: {
+				validators: {
+					notEmpty: {
+						message: 'The email address is required'
+					},
+					emailAddress: {
+						message: 'The email address is not valid'
+					}
+				}
+			},
+			Message: {
+				validators: {
+					notEmpty: {
+						message: 'The Message is required and cannot be empty'
+					}
+				}
+			}
+		}
+	});
+	
     //EFFECT
     
     var controller = $.superscrollorama();
