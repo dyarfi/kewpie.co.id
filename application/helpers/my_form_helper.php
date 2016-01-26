dd4a78019f24cc5d3ce46dc8b6340cf87b18e35b
clutchs <muh.hilman@gmail.com>
2016-01-26 14:18:07 +0700

    chinese new year css

--- application/helpers/my_form_helper.php
+++ application/helpers/my_form_helper.php
@@ -18,26 +18,26 @@ if ( ! function_exists('create_slug'))
 if ( ! function_exists('get_input_admin'))
 {
 	function get_input_admin($code){
-  	$string="
+	  $string="
 	 <div class=\"control-group\">
-     <label class=\"control-label\" for=\"input\">".$code['title']."</label>
-                <div class=\"controls\">
-               <input type=\"".$code['type']."\" class=\"".$code['class']."\" id=\"".$code['id_']."\" name=\"".$code['name']."\">
-                </div>
-                </div>
+	 <label class=\"control-label\" for=\"input\">".$code['title']."</label>
+				<div class=\"controls\">
+			   <input type=\"".$code['type']."\" class=\"".$code['class']."\" id=\"".$code['id_']."\" name=\"".$code['name']."\">
+				</div>
+				</div>
 	";
-   	echo $string;
+	   echo $string;
 	}
 }
 
 if ( ! function_exists('get_date_admin'))
 {
 	function get_date_admin($code){
-		$today = date("d/m/Y");  
+		$today = date("d/m/Y");
 		//echo $today;exit;
-		$string=" 
+		$string="
 		<div class=\"control-group\">
-                <label class=\"control-label\" for=\"input\">".$code['title']."</label>
+				<label class=\"control-label\" for=\"input\">".$code['title']."</label>
 				<div class=\"controls\">
 				<div class=\"input-append\">
 				<input class=\"datepicker input-small\" value=\"".$today."\" type=\"".$code['type']."\" name=\"".$code['name']."\"><span class=\"add-on\"><i class=\"awe-calendar\"></i></span>
@@ -52,11 +52,11 @@ if ( ! function_exists('get_textarea_admin'))
 {
 	function get_textarea_admin($code){
 	$string="  <div class=\"control-group\">
-                <label class=\"control-label\" for=\"input\">".$code['title']."</label>
-                <div class=\"controls\">
-         <textarea class=\"input-xxlarge\" id=\"textarea\" name=\"".$code['name']."\" rows=\"".$code['rows']."\"></textarea>
-                </div>
-                </div>";
+				<label class=\"control-label\" for=\"input\">".$code['title']."</label>
+				<div class=\"controls\">
+		 <textarea class=\"input-xxlarge\" id=\"textarea\" name=\"".$code['name']."\" rows=\"".$code['rows']."\"></textarea>
+				</div>
+				</div>";
 	echo $string;
 	}
 }
@@ -71,24 +71,6 @@ if ( ! function_exists('get_wywisg_admin'))
 				</div>   ";
 		echo $string;
 	}
-	
+
 }
 
-if(! function_exists('get_image_admin'))
-{
-	function get_image_admin($label,$container)
-	{
-		?>
-		<div class="control-group">
-		<label class="control-label" for="fileInput"><?=$label?></label>
-		<div class="controls">
-		<div class="fileupload fileupload-new" data-provides="fileupload">
-		<div class="input-append">
-		<div class="uneditable-input"><i class="icon-file fileupload-exists"></i> <span class="fileupload-preview"></span></div><span class="btn btn-alt btn-file"><span class="fileupload-new">Select file</span><span class="fileupload-exists">Change</span><input type="file" name="<?=$container?>" id="<?=$container?>" /></span><a href="#" class="btn btn-alt btn-danger fileupload-exists" data-dismiss="fileupload">Remove</a>
-		</div>
-		</div>
-        </div>
-	   </div>
-		<?
-	}
-}
